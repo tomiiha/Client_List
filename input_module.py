5c6a53fff3ff7c258732cbd2f9754b4aa200e729
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-12-02 16:31:02 +0000

    Update input_module.py

--- input_module.py
+++ input_module.py
@@ -1,5 +1,17 @@
 # Program for capturing client details
 # Initial version for input, pickling, and exporting
 
+# Import packages
+import pickle
+
+# Dictionaries used
+client_dict = {"first_name":"fname","last_name":"lname","birth_day":"bday",}
+
+# Open up pickle file for data placement
+pickle_base = open("dict.pickle","wb")
+
 # Place commands for operations
-print("Do you want to add a New Entry (E), see Client List (C), Export existing client list (X)")
+print("Do you want to add a New Entry (E), see Client List (C)")
+
+# Close pickle file
+pickle_base.close()
