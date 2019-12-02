f5a31eac58960ad9371d3c4c824478e3024c5d54
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-12-02 16:01:13 +0000

    Add files via upload

---
+++ input_module.py
@@ -0,0 +1,5 @@
+# Program for capturing client details
+# Initial version for input, pickling, and exporting
+
+# Place commands for operations
+print("Do you want to add a New Entry (E), see Client List (C), Export existing client list (X)")
