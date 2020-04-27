3b2927a3c78085e003780a71270d4acb4e7d8b9a
tomiiha <52747351+tomiiha@users.noreply.github.com>
2020-04-27 09:07:26 +0100

    Update client_add.py

--- client_add.py
+++ client_add.py
@@ -1,4 +1,3 @@
-# GUI for adding client instances into DB.
 # Packages
 import sqlite3 as sq
 import tkinter as tk
@@ -9,6 +8,12 @@ try:
     cur = conn.cursor()
 except:
     print("DB Initialization Failed")
+    
+# DB Entry
+def client_add(FName,Phone):
+    print(FName)
+    print(Phone)
+    return 999
 
 # GUI Points
 entry_point = tk.Tk()
@@ -16,12 +21,13 @@ tk.Label(entry_point, text="Full Name").grid(row=0)
 tk.Label(entry_point, text="Phone Number").grid(row=1)
 
 # Capture Variables
-FName = str(tk.Entry(entry_point))
+FName = tk.Entry(entry_point)
 FName.grid(row=0, column=1)
-Phone = str(tk.Entry(entry_point))
+Phone = tk.Entry(entry_point)
 Phone.grid(row=1, column=1)
 
 # GUI Interactibles
 tk.Button(entry_point, text='Quit', command=entry_point.quit).grid(row=3, column=0, sticky=tk.W, pady=4)
+tk.Button(entry_point, text='Submit', command=client_add(FName, Phone)).grid(row=3, column=1, sticky=tk.W, pady=4)
 
 entry_point.mainloop()
